55141f904cac1786d0b2eecd5c3ae87cf5dd4403
Omat-hub <141182163+Omat-hub@users.noreply.github.com>
2025-07-06 11:21:49 +0100

    footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="flex items-start justify-between bg-gray-800  text-sm  py-4 container-padding text-offwhite bg-pantone">
         <p>© 2025 Omatsola Yarumen. All rights reserved.</p>
 
-        <div className="footer-bottom flex flex-col space-y-2">
+        <div className="footer-bottom flex flex-col space-y-2 text-right">
           <p>Designed and Developed by Omatsola Yarumen</p>
           
         </div>
